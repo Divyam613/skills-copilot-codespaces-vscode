06fb7d70ca70ca9662254f4b32188d032d9f78f7
Divyam613 <119338081+Divyam613@users.noreply.github.com>
2023-07-24 13:32:17 +0000

    copilot second commit

--- member.js
+++ member.js
@@ -1,12 +1,12 @@
-function skillsmember() {
-    var skills = ['HTML', 'CSS', 'JS', 'React', 'Node', 'Python', 'Django']
-    var member = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
-    var memberSkills = []
-    for (var i = 0; i < member.length; i++) {
-        memberSkills.push({
-            name: member[i],
-            skills: skills[i]
-        })
-    }
-    return memberSkills
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'modules/skills/views/member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: '='
+        }
+    };
 }
