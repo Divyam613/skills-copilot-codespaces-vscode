7b47ef6fa144487c9998f0b19887af85010c863b
Divyam613 <119338081+Divyam613@users.noreply.github.com>
2023-07-24 13:21:33 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,6 +1,12 @@
 function skillsmember() {
-    var member = document.getElementById("member");
-    var skills = document.getElementById("skills");
-    var contact = document.getElementById("contact");
-    var about = document.getElementById("about");
-    va
+    var skills = ['HTML', 'CSS', 'JS', 'React', 'Node', 'Python', 'Django']
+    var member = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+    var memberSkills = []
+    for (var i = 0; i < member.length; i++) {
+        memberSkills.push({
+            name: member[i],
+            skills: skills[i]
+        })
+    }
+    return memberSkills
+}
